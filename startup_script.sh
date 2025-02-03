0791cec7eaed50fb97eea6f2c24ec5934e55766e
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-03 19:25:36 +0100

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#!/bin/bash
+
+REPO_DIR="/repo"
+GITHUB_REPO="https://github.com/Th0mmi3/auto_comment.git"
+SCRIPT_TO_RUN="wallet_generator.py"
 sudo apt-get update
-sudo apt-get install -y git python3-pip
-if [ ! -d "{REPO_DIR}" ]; then
-    git clone {GITHUB_REPO} {REPO_DIR}
+sudo apt-get install -y git python3.9 python3.9-distutils python3.9-venv
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+if [ ! -d "${REPO_DIR}" ]; then
+    git clone "${GITHUB_REPO}" "${REPO_DIR}"
 fi
-cd {REPO_DIR}
-pip3 install -r requirements.txt
-python3 {SCRIPT_TO_RUN} &
+cd "${REPO_DIR}"
+
+python3.9 -m pip install -r requirements.txt
+python3.9 "${SCRIPT_TO_RUN}" &
