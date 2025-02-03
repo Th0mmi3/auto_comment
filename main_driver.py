78729e6755c6e2be5fea30bf2e15e83a7ec5f6d6
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2025-02-03 18:37:16 +0100

    Create main_driver.py

---
+++ main_driver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import bulk_vps_create
+
+def main():
+    print("Start bulk VPS-creatie met wallet generatie...")
+    bulk_vps_create.main()
+    print("Bulk VPS-creatie is gestart.")
+
+if __name__ == "__main__":
+    main()
